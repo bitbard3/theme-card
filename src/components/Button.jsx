6914f6b2760e66365ef8d1d0b50c1ed98d9a0e17
bitbard3 <ansharora3839@gmail.com>
2024-01-31 19:20:45 +0530

    Add fucntionality to toggle theme mode

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,23 @@
 import { SunIcon } from "@heroicons/react/16/solid";
+import { MoonIcon } from "@heroicons/react/16/solid";
+import { useTheme } from "../contexts/Theme";
+import { useState } from "react";
+
 export const Button = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const { theme, lightMode, darkMode } = useTheme();
+  if (darkTheme) {
+    darkMode();
+  } else {
+    lightMode();
+  }
   return (
-    <button>
-      <SunIcon className="h-8 w-8 text-accent" />
+    <button onClick={() => setDarkTheme(!darkTheme)}>
+      {darkTheme ? (
+        <MoonIcon className="h-8 w-8 text-primary"></MoonIcon>
+      ) : (
+        <SunIcon className="h-8 w-8 text-accent" />
+      )}
     </button>
   );
 };
