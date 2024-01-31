5221f3e0b3e04e34cc7e6623eded78be341986ef
bitbard3 <ansharora3839@gmail.com>
2024-01-31 19:21:08 +0530

    Add fucntionality to toggle theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
+import { ThemeProvider } from "./contexts/Theme";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const darkMode = () => {
+    setTheme("dark");
+  };
+  const lightMode = () => {
+    setTheme("light");
+  };
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(theme);
+  }, [theme]);
   return (
     <>
-      <div className="">
-        <div className="flex justify-center items-center h-screen">
-          <Card></Card>
+      <ThemeProvider value={{ theme, darkMode, lightMode }}>
+        <div className="">
+          <div className="flex justify-center items-center h-screen">
+            <Card></Card>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
